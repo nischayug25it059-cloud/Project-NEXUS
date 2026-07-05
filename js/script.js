5d8f290d306e34649a1e9e6a4b366cf4417ecf3d
Nischay Nayak <nischay.ug25it059@bitsindri.ac.in>
2026-07-05 22:52:05 +0530

    Complete Portfolio V1

--- js/script.js
+++ js/script.js
@@ -30,3 +30,35 @@ p.style.animationDelay=(Math.random()*10)+"s";
 particleContainer.appendChild(p);
 
 }
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (scrollY >= sectionTop) {
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
